cb4ca0ce17ebff7b1b1766b89ec9c9ff8def955d
mukundha@apigee.com <mukundha@apigee.com>
2016-08-28 22:39:18 -0700

    add proxy name to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var apigeetool = require('apigeetool')
 var gutil = require('gulp-util')
-
+var proxy_name = 'apigee-sample-quota-basic'
 gulp.task('default', function() {
   // place code for your default task here
 });
@@ -14,6 +14,7 @@ var opts = {
 }
 
 gulp.task('deploy',function(){
+	opts.api = proxy_name
 	return apigeetool.getPromiseSDK()
 		.deployProxy(opts)
 })
